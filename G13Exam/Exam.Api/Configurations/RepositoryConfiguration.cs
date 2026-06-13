530dfb02e00082dc757e4399ebbc5e738657dd38
bunyodmusayev17-beep <bunyodmusayev17@gmail.com>
2026-06-13 17:25:27 +0500

    WI8 Food repository implementatsiyasi done

---
+++ G13Exam/Exam.Api/Configurations/RepositoryConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Exam.Api.Repositories;
+
+namespace Exam.Api.Configurations;
+
+public static class RepositoryConfiguration
+{
+    public static void ConfigureRepository(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+        builder.Services.AddScoped<IFoodRepository, FoodRepository>();
+    }
+}
--- G13Exam/Exam.Api/Repositories/FoodRepository.cs
+++ G13Exam/Exam.Api/Repositories/FoodRepository.cs
@@ -1,12 +1,61 @@
 using Exam.Api.Data;
 using Exam.Api.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Exam.Api.Repositories;
 
-public class FoodRepository : BaseRepository<Food>, IFoodRepository
+public class FoodRepository : IFoodRepository
 {
-    public FoodRepository(AppDbContext dbContext)
-        : base(dbContext)
+
+
+    private readonly AppDbContext _context;
+
+    public FoodRepository(AppDbContext context)
     {
+        _context = context;
+    }
+
+    public async Task<List<Food>> GetAllAsync()
+    {
+        return await _context.Foods
+            .Include(f => f.Category)
+            .ToListAsync();
+    }
+
+    public async Task<Food?> GetByIdAsync(long id)
+    {
+        return await _context.Foods
+            .Include(f => f.Category)
+            .FirstOrDefaultAsync(f => f.FoodId == id);
+    }
+
+    public async Task<Food> CreateAsync(Food food)
+    {
+        await _context.Foods.AddAsync(food);
+        await _context.SaveChangesAsync();
+
+        return food;
+    }
+
+    public async Task<Food> UpdateAsync(Food food)
+    {
+        _context.Foods.Update(food);
+        await _context.SaveChangesAsync();
+
+        return food;
+    }
+
+    public async Task<bool> DeleteAsync(long id)
+    {
+        var food = await _context.Foods
+            .FirstOrDefaultAsync(f => f.FoodId == id);
+
+        if (food is null)
+            return false;
+
+        _context.Foods.Remove(food);
+        await _context.SaveChangesAsync();
+
+        return true;
     }
 }
