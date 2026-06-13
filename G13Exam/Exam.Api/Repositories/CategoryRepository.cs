eca5afaccb7ad950ad2cf880dd95f3d667b88a3b
bunyodmusayev17-beep <bunyodmusayev17@gmail.com>
2026-06-13 16:58:41 +0500

    WI7 Category repository implementatsiyasi   done

---
+++ G13Exam/Exam.Api/Repositories/CategoryRepository.cs
@@ -0,0 +1,34 @@
+﻿using Exam.Api.Data;
+using Exam.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Exam.Api.Repositories;
+
+public class CategoryRepository : ICategoryRepository
+{
+    private readonly AppDbContext _context;
+
+    public CategoryRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Category>> GetAllAsync()
+    {
+        return await _context.Categories.ToListAsync();
+    }
+
+    public async Task<Category?> GetByIdAsync(long id)
+    {
+        return await _context.Categories
+            .FirstOrDefaultAsync(c => c.CategoryId == id);
+    }
+
+    public async Task<Category> CreateAsync(Category category)
+    {
+        await _context.Categories.AddAsync(category);
+        await _context.SaveChangesAsync();
+
+        return category;
+    }
+}
---
+++ G13Exam/Exam.Api/Repositories/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using Exam.Api.Entities;
+
+namespace Exam.Api.Repositories;
+
+
+public interface ICategoryRepository
+{
+    Task<List<Category>> GetAllAsync();
+    Task<Category?> GetByIdAsync(long id);
+    Task<Category> CreateAsync(Category category);
+}
