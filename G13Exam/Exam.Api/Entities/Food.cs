f83ca8fd571aac2205b0ec173eec913be60d8c38
bunyodmusayev17-beep <bunyodmusayev17@gmail.com>
2026-06-13 15:47:14 +0500

    WI6 Custom mapper servislarini yaratildi

--- G13Exam/Exam.Api/Entities/Food.cs
+++ G13Exam/Exam.Api/Entities/Food.cs
@@ -3,13 +3,13 @@
 public class Food
 {
     public long FoodId { get; set; }
-    public string name { get; set; }
+    public string Name { get; set; }
     public string Discription { get; set; }
     public decimal Price { get; set; }
     public bool IsAvailable { get; set; }
 
   
     // Navigation prop
-    public ICollection<Category> Categories { get; set; }
+    public Category Category { get; set; }
     public long CategoryId { get; set; }
 }
---
+++ G13Exam/Exam.Api/Mappings/CategoryMapper.cs
@@ -0,0 +1,29 @@
+﻿using Exam.Api.Dtos;
+using Exam.Api.Entities;
+
+namespace Exam.Api.Mappings;
+
+public static class CategoryMapper
+{
+    public static CategoryDto ToDto(this Category category)
+    {
+        return new CategoryDto
+        {
+            CategoryId = (int)category.CategoryId,
+            Name = category.Name
+        };
+    }
+
+    public static Category ToEntity(this CategoryCreateDto dto)
+    {
+        return new Category
+        {
+            Name = dto.Name
+        };
+    }
+
+    public static void ToUpdateEntity(this Category category, CategoryUpdateDto dto)
+    {
+        category.Name = dto.Name;
+    }
+}
